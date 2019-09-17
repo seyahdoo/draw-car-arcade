5e820ad9f7cca239ebc3542955c576a488f0f0c4
Seyyid Ahmed Doğan <contact@seyahdoo.com>
2019-09-17 14:09:28 +0300

    cube drawer

---
+++ Assets/DrawSurface.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawSurface : MonoBehaviour
+{
+
+    Vector3 pos;
+
+    bool recorded = false;
+
+    public Camera cam;
+    public LayerMask mask;
+    readonly RaycastHit[] hits = new RaycastHit[1];
+
+    public CubeDrawer drawer;
+
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.RaycastNonAlloc(ray, hits, 100f, mask) > 0)
+            {
+                Debug.Log("dasd");
+
+                GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                go.transform.localScale = Vector3.one * .2f;
+                go.transform.position = hits[0].point;
+
+                if (!recorded)
+                {
+                    recorded = true;
+                    pos = hits[0].point;
+                }
+                else
+                {
+                    recorded = false;
+                    drawer.DrawCube(pos, hits[0].point, .1f, Vector3.up);
+                }
+
+            }
+
+
+
+        }
+
+        
+
+    }
+
+
+
+}
---
+++ Assets/Scripts/CubeDrawer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeDrawer : MonoBehaviour
+{
+
+    public GameObject prefab;
+
+    public void DrawCube(Vector3 pos1, Vector3 pos2, float thiccnes, Vector3 up)
+    {
+        GameObject go = Instantiate(prefab);
+
+        go.transform.position = pos1;
+        go.transform.localScale = new Vector3(thiccnes, thiccnes, Vector3.Distance(pos1, pos2));
+        go.transform.LookAt(pos2, up);
+
+    }
+
+
+
+}
